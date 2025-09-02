0277181ed73167b37166a646e528e7d865a0b504
sarvar-devx <sarvarbekdavranbekov@gmail.com>
2025-09-02 18:12:51 +0500

    add routers

--- bot/handlers/main.py
+++ bot/handlers/main.py
@@ -1,4 +1,15 @@
-from aiogram import Router, Bot
+from aiogram import Router, Bot, F
 from aiogram.types import Message
 
+from bot.keyboard.reply import UserButtons
+from config import conf
+
 main_router = Router()
+
+
+@main_router.message(F.text == UserButtons.OPERATOR)
+async def show_operator_handler(message: Message):
+    await message.answer(
+        f'👨‍💼 Admin {conf.bot.OPERATOR_NUMBER}')
+
+
