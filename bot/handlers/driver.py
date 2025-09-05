40fe6248725a8675a1178e153c3d0c57de7b852b
artyom <artyom.bw08@gmail.com>
2025-09-06 00:35:31 +0500

    Driver update car_brand

---
+++ bot/handlers/driver.py
@@ -0,0 +1,29 @@
+from aiogram import Router, F
+from aiogram.filters import StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, ReplyKeyboardRemove
+
+from bot.keyboard.reply import DriverButtons
+from bot.states.user import DriverStates
+from db import Driver
+
+driver_router = Router()
+
+
+@driver_router.message(F.text == DriverButtons.CHANGE_CAR_BRAND, StateFilter(None))
+async def update_car_brand_handler(message: Message, state: FSMContext):
+    await message.answer("📝 Yangi model nomini kiriting", reply_markup=ReplyKeyboardRemove())
+    await state.set_state(DriverStates.car_brand)
+
+
+@driver_router.message(DriverStates.car_brand)
+async def change_car_brand_handler(message: Message, state: FSMContext):
+    if not message.text.split():
+        await message.answer("‼️ Mashina brendi bosh bolishi mumkun emas qaytadan kiriting")
+        return
+    await Driver.update(message.from_user.id, car_brand=message.text.title())
+    await message.answer(f"✅ Mashina brandi {message.text.title()}ga ozgartirildi !")
+    await state.clear()
+
+
+
