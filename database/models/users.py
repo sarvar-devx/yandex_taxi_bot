df8b133b2c1c9fd8c0fc141226c4bcdafc7eac05
sarvar-devx <sarvarbekdavranbekov@gmail.com>
2025-09-16 12:29:29 +0500

    went back

--- database/models/users.py
+++ database/models/users.py
@@ -19,6 +19,8 @@ class User(TimeBaseModel):
 
 # Userni borish manzili
 class Address(TimeBaseModel):
+    __tablename__ = "addresses"
+
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     user: Mapped["User"] = relationship("User", back_populates="addresses")
 
